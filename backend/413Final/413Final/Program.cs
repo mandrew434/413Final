977a89801b4db10ec91c91aa761a68168ca354f3
Andrew Malone <mandrew434@gmail.com>
2025-04-21 13:45:41 -0600

    Commit made from VS Code Azure Static Web Apps

--- backend/413Final/413Final/Program.cs
+++ backend/413Final/413Final/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000, ")
                 .AllowCredentials()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
